c2ad5f6f17670be4531d54892b12c3527551246d
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-23 11:42:50 +0900

    fontAusomeインストール

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faAngleLeft} from "@fortawesome/free-solid-svg-icons";
+
+const SideBar: React.FC = () => {
+  return (
+    <Wrapper>
+      <BackButton>
+        <BackIcon icon={faAngleLeft} />
+      </BackButton>
+    </Wrapper>
+  );
+};
+
+export default SideBar;
+
+const Wrapper = styled.nav`
+  width: 80px;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+`;
+
+const BackButton = styled.button`
+  outline: none;
+  border: none;
+  cursor: pointer;
+`;
+
+const BackIcon = styled(FontAwesomeIcon)`
+  font-size: 50px;
+  color: #808080;
+`
