a9d1ff12faecb621f5447efa0576a078610a6574
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-26 18:29:14 +0900

    投稿詳細の情報修正

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -4,7 +4,7 @@ import { CSSTransition } from "react-transition-group";
 import { TransitionStatus } from "react-transition-group/Transition";
 import GoogleMapReact from "google-map-react";
 import moment from "moment";
-import { usePostQuery } from "../gen/graphql-client-api";
+import { usePostQuery, Visitor } from "../gen/graphql-client-api";
 import * as CSS from "../commonStyles";
 
 interface Props {
@@ -59,7 +59,9 @@ const Post: React.FC<Props> = props => {
               <Pin
                 lat={data?.post.location?.lat!}
                 lng={data?.post.location?.lng!}
-              />
+              >
+                ここ
+              </Pin>
             </GoogleMapReact>
           </MapWrapper>
         )}
@@ -72,49 +74,52 @@ const Post: React.FC<Props> = props => {
       >
         {status => (
           <Wrap transitionStatus={status}>
-            {(data?.post.visitors as {
-              visitorName: string;
-              image: string;
-              discription: string;
-            }[]).length > 0 && (
-              <VisitorsWrap>
-                <VisitorTitle>こんな人が来ました</VisitorTitle>
-                {(data?.post.visitors as {
-                  visitorName: string;
-                  image: string;
-                  discription: string;
-                }[]).map(visitor => (
-                  <VisitorItemWrap key={visitor.image}>
-                    <VisitorImage
-                      src={`${BUCKET_URL}/${visitor.image}`}
-                      alt="訪問者イメージ"
-                    />
-                    <VisitorName>{visitor.visitorName}</VisitorName>
-                    <VisitorDiscription>
-                      {visitor.discription}
-                    </VisitorDiscription>
-                  </VisitorItemWrap>
-                ))}
-              </VisitorsWrap>
-            )}
             <PostSumbnail sumbnailUrl={data?.post.sumbnail!} />
-            <PostName>{data?.post.name}</PostName>
-            <DateTimeWrap>
-              <DateTime>{moment(data?.post.start).format("YYYY/MM/DD hh:mm")}</DateTime>
-              <Hyphen />
-              <DateTime>{moment(data?.post.finish).format("YYYY/MM/DD hh:mm")}</DateTime>
-            </DateTimeWrap>
-            <Discription>{data?.post.discription}</Discription>
-            <Address>{data?.post.address}</Address>
-            <PostImagesWrap>
-              {(data?.post.images! as string[]).map(image => (
-                <PostImage
-                  key={image}
-                  src={`${BUCKET_URL}/${image}`}
-                  alt="イベントイメージ"
-                />
-              ))}
-            </PostImagesWrap>
+            <PostInfoWrapper>
+              <PostName>{data?.post.name}</PostName>
+              {(data?.post.visitors as {
+                visitorName: string;
+                image: string;
+                discription: string;
+              }[]).length > 0 && (
+                <VisitorsWrap>
+                  <VisitorTitle>こんな人が来ました</VisitorTitle>
+                  {(data?.post.visitors as Visitor[]).map(visitor => (
+                    <VisitorItemWrap key={visitor.sumbnail!}>
+                      <VisitorImage
+                        src={`${BUCKET_URL}/${visitor.sumbnail}`}
+                        alt="訪問者イメージ"
+                      />
+                      <VisitorName>{visitor.visitorName}</VisitorName>
+                      <VisitorDiscription>
+                        {visitor.discription}
+                      </VisitorDiscription>
+                    </VisitorItemWrap>
+                  ))}
+                </VisitorsWrap>
+              )}
+              <DateTimeWrap>
+                <DateTime beforeContent="Start">
+                  {moment(data?.post.start).format("YYYY/MM/DD hh:mm")}
+                </DateTime>
+              </DateTimeWrap>
+              <DateTimeWrap>
+                <DateTime beforeContent="Finish">
+                  {moment(data?.post.finish).format("YYYY/MM/DD hh:mm")}
+                </DateTime>
+              </DateTimeWrap>
+              <Discription>{data?.post.discription}</Discription>
+              <Address>{data?.post.address}</Address>
+              <PostImagesWrap>
+                {(data?.post.images! as string[]).map(image => (
+                  <PostImage
+                    key={image}
+                    src={`${BUCKET_URL}/${image}`}
+                    alt="イベントイメージ"
+                  />
+                ))}
+              </PostImagesWrap>
+            </PostInfoWrapper>
           </Wrap>
         )}
       </CSSTransition>
@@ -128,7 +133,7 @@ const MapWrapper = styled.div`
   width: calc(100vw - ${CSS.SideBarWidth});
   height: 100vh;
   position: fixed;
-  z-index: 2;
+  z-index: 4;
   top: 0;
   left: ${CSS.SideBarWidth};
 
@@ -140,12 +145,12 @@ const MapWrapper = styled.div`
         `;
       case "entered":
         return css`
-          opacity: 0.7;
+          opacity: 1;
           transition: opacity ${MAP_TRANDISION_DURATION}ms ease;
         `;
       case "exited":
         return css`
-          opacity: 0.7;
+          opacity: 1;
         `;
       case "exiting":
         return css`
@@ -168,7 +173,7 @@ const Wrap = styled.div`
   border-radius: 10px;
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
   position: fixed;
-  z-index: 3;
+  z-index: 5;
   bottom: 92px;
   left: calc(50vw - (872px / 2));
 
@@ -213,6 +218,14 @@ const PostSumbnail = styled.figure`
   `}
 `;
 
+const PostInfoWrapper = styled.div`
+  width: 100%;
+  height: 569px;
+  padding: 50px;
+  overflow-y: scroll;
+  box-sizing: border-box;
+`;
+
 const PostName = styled.h2`
   font-size: 45px;
   color: #707070;
@@ -220,20 +233,28 @@ const PostName = styled.h2`
 
 const DateTimeWrap = styled.div`
   display: flex;
-  justify-content: flex-end;
+  align-items: center;
 `;
 
 const DateTime = styled.time`
   font-size: 35px;
+  display: block;
   color: #707070;
-`;
 
-const Hyphen = styled.span`
   &::before {
-    content: "-";
-    font-size: 35px;
-    color: #707070;
-    margin: 0 20px;
+    content: "${(props: { beforeContent: string }) => props.beforeContent}";
+    width: 50px;
+    height: 20px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+    background: #f00;
+    color: #fff;
+    border-color: 5px;
+    border-radius: 5px;
+    padding: 5px;
+    margin-right: 10px;
   }
 `;
 
@@ -250,12 +271,10 @@ const PostImage = styled.img`
   height: 270px;
   box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.2);
   object-fit: cover;
-  margin: 0 40px; 
+  margin: 0 40px;
 `;
 
-const Address = styled.p`
-
-`;
+const Address = styled.p``;
 
 const VisitorsWrap = styled.div`
   margin-bottom: 35px;
@@ -271,6 +290,7 @@ const VisitorItemWrap = styled.div``;
 const VisitorImage = styled.img`
   width: 343px;
   height: 223px;
+  object-fit: cover;
 `;
 
 const VisitorName = styled.h4`
