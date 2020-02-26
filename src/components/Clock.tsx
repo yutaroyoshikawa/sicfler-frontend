96444125ac8ae78b8d5835a60eac5835399ee9dc
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-26 10:56:35 +0900

    日付実装

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -12,14 +12,35 @@ const Clock: React.FC = () => {
   }, [currentTime]);
 
   return (
-    <Time>{moment(currentTime).format("HH:mm")}</Time>
+    <Wrapper>
+      <Calender>
+        {moment(currentTime).format("MM/DD")}
+      </Calender>
+      <Time>{moment(currentTime).format("HH:mm")}</Time>
+    </Wrapper>
   );
 };
 
 export default Clock;
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Calender = styled.p`
+  font-size: 26px;
+  color: #707070;
+  display: inline-block;
+  text-align: left;
+  letter-spacing: 5px;
+  margin-bottom: 10px;
+`;
+
 const Time = styled.time`
   font-size: 30px;
+  color: #303030;
   display: inline-block;
   text-align: center;
   letter-spacing: 5px;
